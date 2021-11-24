fb597f318bb1b2c88b37ced83a6b75c030b21a31
Federico Piccirilli <federicopicci@gmail.com>
2021-11-24 11:19:09 +0000

    Update Bag

--- Bullet/Model/Boardgame/Bag.swift
+++ Bullet/Model/Boardgame/Bag.swift
@@ -31,56 +31,18 @@ struct GameComponents {
     struct Bullets {
         static let center: [Bullet] = {
             var bag = [Bullet]()
-            bag +=  Array(repeating: Bullet(color: .red, value: 4), count: 2)
-            bag +=  Array(repeating: Bullet(color: .red, value: 3), count: 6)
-            bag +=  Array(repeating: Bullet(color: .red, value: 2), count: 6)
-            bag +=  Array(repeating: Bullet(color: .red, value: 1), count: 6)
+            let colors: [Bullet.TokenType] = [.red, .blue, .green, .yellow, .pink ]
+            for color in colors {
+                bag +=  Array(repeating: Bullet(color: color, value: 4), count: 2)
+                bag +=  Array(repeating: Bullet(color: color, value: 3), count: 6)
+                bag +=  Array(repeating: Bullet(color: color, value: 2), count: 6)
+                bag +=  Array(repeating: Bullet(color: color, value: 1), count: 6)
 
-            bag +=  Array(repeating: Bullet(color: .red, value: 4, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .red, value: 3, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .red, value: 2, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .red, value: 1, star: true), count: 2)
-
-            bag +=  Array(repeating: Bullet(color: .blue, value: 4), count: 2)
-            bag +=  Array(repeating: Bullet(color: .blue, value: 3), count: 6)
-            bag +=  Array(repeating: Bullet(color: .blue, value: 2), count: 6)
-            bag +=  Array(repeating: Bullet(color: .blue, value: 1), count: 6)
-
-            bag +=  Array(repeating: Bullet(color: .blue, value: 4, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .blue, value: 3, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .blue, value: 2, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .blue, value: 1, star: true), count: 2)
-
-            bag +=  Array(repeating: Bullet(color: .green, value: 4), count: 2)
-            bag +=  Array(repeating: Bullet(color: .green, value: 3), count: 6)
-            bag +=  Array(repeating: Bullet(color: .green, value: 2), count: 6)
-            bag +=  Array(repeating: Bullet(color: .green, value: 1), count: 6)
-
-            bag +=  Array(repeating: Bullet(color: .green, value: 4, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .green, value: 3, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .green, value: 2, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .green, value: 1, star: true), count: 2)
-
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 4), count: 2)
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 3), count: 6)
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 2), count: 6)
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 1), count: 6)
-
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 4, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 3, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 2, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .yellow, value: 1, star: true), count: 2)
-
-            bag +=  Array(repeating: Bullet(color: .pink, value: 4), count: 2)
-            bag +=  Array(repeating: Bullet(color: .pink, value: 3), count: 6)
-            bag +=  Array(repeating: Bullet(color: .pink, value: 2), count: 6)
-            bag +=  Array(repeating: Bullet(color: .pink, value: 1), count: 6)
-
-            bag +=  Array(repeating: Bullet(color: .pink, value: 4, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .pink, value: 3, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .pink, value: 2, star: true), count: 2)
-            bag +=  Array(repeating: Bullet(color: .pink, value: 1, star: true), count: 2)
-
+                bag +=  Array(repeating: Bullet(color: color, value: 4, star: true), count: 2)
+                bag +=  Array(repeating: Bullet(color: color, value: 3, star: true), count: 2)
+                bag +=  Array(repeating: Bullet(color: color, value: 2, star: true), count: 2)
+                bag +=  Array(repeating: Bullet(color: color, value: 1, star: true), count: 2)
+            }
             return bag
         }()
     }
